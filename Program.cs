e68762a7fa6c48d1f286b87905d99244b3d645b6
AKzar1el <tomi.seregi99@gmail.com>
2023-03-20 17:25:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// import a namespace called System 
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        // This function can run before
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+
+            // Data types
+            bool canI = true; //false
+
+            int biggestInt = int.MaxValue;
+
+            long biggestLong = long.MaxValue;
+
+            decimal biggestDecimal = decimal.MaxValue;
+
+
+        }
+    }
+}
